ab0b74990702e7bb5a185f7aef13ae762b37467f
BhryanS2 <bhryanstepenhen@gmail.com>
2022-12-15 08:44:00 -0300

    add logger

--- poc_observabilidade/src/index.ts
+++ poc_observabilidade/src/index.ts
@@ -1,4 +1,5 @@
 import "./tracer";
+import { Logger } from "./logger";
 
 import express from "express";
 import crypto from "node:crypto";
@@ -6,17 +7,21 @@ import crypto from "node:crypto";
 const app = express();
 
 app.use(express.json());
+const logger = new Logger();
 
 app.post("/user", (req, res) => {
+  logger.log("info", "Creating user");
   const { token } = req.headers;
 
   const { name, email } = req.body;
 
   if (!token) {
+    logger.log("error", "Token not found");
     return res.status(401).end();
   }
 
   if (!name || !email) {
+    logger.log("error", "Fields requireds");
     return res.status(400).json({
       error: "fields requireds",
       fields: {
@@ -32,13 +37,17 @@ app.post("/user", (req, res) => {
     email,
   };
 
+  logger.log("info", "User created");
+
   return res.json(user).end();
 });
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+  logger.log("info", "Hello World");
+  return res.json({ message: "Hello World" });
 });
 
 app.listen(3333, () => {
+  logger.log("info", "Server running on port 3333");
   console.log("Server running on port 3333");
 });
